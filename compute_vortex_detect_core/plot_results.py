6e8d61a6f3e3cc58eebd5523bed2d190dfb528c7
Patrick G.C. Deng <patrickgc.deng@mail.utoronto.ca>
2025-08-19 18:49:51 -0600

    updated results plotting

--- compute_vortex_detect_core/plot_results.py
+++ compute_vortex_detect_core/plot_results.py
@@ -324,7 +324,7 @@ def plot_vortex_cores(cut_loc, output_dir, chord=0.3048, data_type='LES'):
                 bbox_inches='tight', pad_inches=0.2)
     plt.close()
     # Extract PCA line data and save to HDF5 (commented out - obsolete)
-    filename = os.path.join('./', f'Velocity_Core_B_10AOA_U30'+{data_type}+'.h5')
+    filename = os.path.join('./', f'Velocity_Core_B_10AOA_U30_{data_type}.h5')
     extract_pca_line(P_core_loc_scaled, y, z, u, v, w, vort, filename, cut_loc, 'PV')
     extract_pca_line(S_core_loc_scaled, y, z, u, v, w, vort, filename, cut_loc, 'SV')
     if tertiary and len(T_core_loc) > 0:
